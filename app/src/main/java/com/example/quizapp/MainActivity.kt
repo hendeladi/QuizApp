2bc7be860cc4a13bad288088941ba72d87b62070
adihendel <hendeladi@gmail.com>
2020-08-29 22:04:32 +0300

    added activities and buttons

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun quizStartClicked(view: View){
+        val takeQuizIntent = Intent(this,TakeQuizActivity::class.java)
+        startActivity(takeQuizIntent)
+    }
+
+    fun addQuestionClicked(view: View){
+        val addQuestionIntent = Intent(this,AddQuestionActivity::class.java)
+        startActivity(addQuestionIntent)
+    }
+
+    fun viewQuestionClicked(view: View){
+
     }
 }
---
+++ app/src/main/java/com/example/quizapp/TakeQuizActivity.kt
@@ -0,0 +1,20 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class TakeQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_quiz)
+    }
+
+    fun showAnswerClicked(view: View){
+
+    }
+
+    fun nextQuestionClicked(view: View){
+
+    }
+}
